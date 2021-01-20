d97ff69ae32f604955f4c1535ad9729b0f3fd33d
donovan-herion <donovanherion@gmail.com>
2021-01-20 11:15:46 +0100

    second version

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ let incrementBtn = document.querySelector(".increment");
 let decrementBtn = document.querySelector(".decrement");
 let containerChildren = document.querySelector(".container").children;
 
+let fontSizeStatus = 1;
+
 incrementBtn.addEventListener("click", () => {
-  for (let i = 0; i < containerChildren.length; i++) {
-    let fontSize = window.getComputedStyle(containerChildren[i]).fontSize;
-    containerChildren[i].style.fontSize = `${parseInt(fontSize) * 1.1}px`;
+  if (fontSizeStatus < 2) {
+    decrementBtn.style.cursor = "pointer";
+    for (let i = 0; i < containerChildren.length; i++) {
+      let fontSize = window.getComputedStyle(containerChildren[i]).fontSize;
+      containerChildren[i].style.fontSize = `${parseInt(fontSize) + 5}px`;
+    }
+    fontSizeStatus++;
+    if (fontSizeStatus == 2) {
+      incrementBtn.style.cursor = "no-drop";
+    }
   }
 });
 
 decrementBtn.addEventListener("click", () => {
-  for (let i = 0; i < containerChildren.length; i++) {
-    let fontSize = window.getComputedStyle(containerChildren[i]).fontSize;
-    containerChildren[i].style.fontSize = `${parseInt(fontSize) * 0.9}px`;
+  if (fontSizeStatus > 0) {
+    incrementBtn.style.cursor = "pointer";
+    for (let i = 0; i < containerChildren.length; i++) {
+      let fontSize = window.getComputedStyle(containerChildren[i]).fontSize;
+      containerChildren[i].style.fontSize = `${parseInt(fontSize) - 5}px`;
+    }
+    fontSizeStatus--;
+
+    if (fontSizeStatus == 0) {
+      decrementBtn.style.cursor = "no-drop";
+    }
   }
 });
